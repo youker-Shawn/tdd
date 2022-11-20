6102f3f87bbea936260a94d55625725d26342b7f
youker-Shawn <youkerbz@gmail.com>
2022-11-20 22:16:05 +0800

    add comments about telling a user story to describe the function

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,26 @@
 from selenium import webdriver
+
 browser = webdriver.Firefox()
+
+# Someone heard about a cool online to-do app. He goes to check out its homepage
 browser.get("http://localhost:8000")
 
-assert "Django" in browser.title
+# He notices the page title and the header mention to-do lists
+assert "To-Do" in browser.title
+
+# He wants to enter a to-do item straight away.
+
+# So he types "Buy eggs" into a text box.
+
+# When he hits enter, the page updates, and now the page lists "1: Buy eggs" as an item in to-do list.
+
+# There is still a text box inviting he to add another item to the to-do list. So he enters "Learn TDD".
+
+# The page updates again, and now shows two items on his list.
+
+# The site has generated a uniqe URL for him.
+
+# He visites the URL and his to-do list is still there.
+
+# Satisfied, he closes the browser.
+browser.quit()
