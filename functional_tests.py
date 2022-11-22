0ceb8aa9c5b5a806c7e46c39bef8687f63209813
youker-Shawn <youkerbz@gmail.com>
2022-11-22 10:48:59 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -38,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')  
         self.assertTrue(
-            any(row.text == '1: Buy eggs' for row in rows)
+            any(row.text == '1: Buy eggs' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting he to add another item to the to-do list. So he enters "Learn TDD".
